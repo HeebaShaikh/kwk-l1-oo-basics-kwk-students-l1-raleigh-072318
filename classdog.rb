5476be4070266293e188138835db46aaa1fdfb61
HeebaShaikh <heeba126@gmail.com>
2018-07-26 18:34:49 +0000

    automatically backed up by learn

---
+++ classdog.rb
@@ -0,0 +1,26 @@
+class dog 
+  def initialize (name, breed)
+    @name = name 
+    @breed = breed 
+  end 
+  
+  #getter for instance attribute name
+  def name 
+    @name 
+  end 
+  
+  # getter for instance attribute breed 
+  def breed 
+    @breed 
+  end 
+  
+  #setter for instance attribute name 
+  def name=(name)
+    @name = name 
+  end 
+  
+  # setter for instance attribute breed 
+  def breed=(breed)
+    @breed = breed 
+  end 
+end 
